5eb026913329b8c29ad9c0eeb041bf45b36211b6
Recordu45 <135110495+Recordu45@users.noreply.github.com>
2026-08-15 01:05:42 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -17,11 +18,18 @@ android {
         versionName = "1.0"
     }
 
+    buildFeatures {
+        compose = true
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
+
             proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
+                getDefaultProguardFile(
+                    "proguard-android-optimize.txt"
+                ),
                 "proguard-rules.pro"
             )
         }
@@ -38,15 +46,26 @@ android {
 }
 
 dependencies {
+
     implementation("androidx.core:core-ktx:1.15.0")
+
     implementation("androidx.activity:activity-compose:1.10.1")
 
     implementation("androidx.compose.ui:ui:1.7.8")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.7.8")
 
-    implementation("androidx.compose.material3:material3:1.3.1")
+    implementation(
+        "androidx.compose.ui:ui-tooling-preview:1.7.8"
+    )
 
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
+    implementation(
+        "androidx.compose.material3:material3:1.3.1"
+    )
 
-    debugImplementation("androidx.compose.ui:ui-tooling:1.7.8")
+    implementation(
+        "androidx.lifecycle:lifecycle-runtime-compose:2.8.7"
+    )
+
+    debugImplementation(
+        "androidx.compose.ui:ui-tooling:1.7.8"
+    )
 }
